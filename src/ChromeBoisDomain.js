7a3f5ccf53d91222c2a91503eb25da51ac700d45
Alan Krajina <alankrajina88@gmail.com>
2019-10-17 11:42:34 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -12,7 +12,7 @@ export default class ChromeBoisDomain extends Component {
 
   clicker = () => {
     return (
-      <canvas onClick={() => toggleCycling())}>
+      <canvas onClick={() => toggleCycling()}>
         Click me!
       </canvas>
     );
